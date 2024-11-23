027acefcece4a8d94d0691c44867650dccd5df52
student <student@ur.edu.pl>
2024-11-23 15:20:34 +0100

    Sprawdzenie działania quick sort

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "signum.c"
+#include "quick_sort.c"
 
 // rm -rf * && rm -rf .* && git clone https://github.com/Yohop/algo-mat.git .
 
@@ -17,5 +18,11 @@ int main()
     printf("%d\n", signum(0));
     printf("%d\n", signum(5));
 
+    int n = 6;
+
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    quick_sort(arr, 0, n - 1);
+    print_array(arr, n);
+    
     return 0;
 }
