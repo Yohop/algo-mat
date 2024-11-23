77c227d9ddac284722c87ba21f6950e9d7f9ef8a
student <student@ur.edu.pl>
2024-11-23 15:19:03 +0100

    Sprawdzenie działania insertion sort

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "signum.c"
+#include "insertion_sort.c"
 
 // rm -rf * && rm -rf .* && git clone https://github.com/Yohop/algo-mat.git .
 
@@ -16,6 +17,12 @@ int main()
     printf("%d\n", signum(-5));
     printf("%d\n", signum(0));
     printf("%d\n", signum(5));
+
+    int n = 6;
+
+    int arr[] = {2, 9, 3, 6, 7, 1};
+    insertion_sort(arr, n);
+    print_array(arr, n);
 
     return 0;
 }
