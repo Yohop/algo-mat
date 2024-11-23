b02a263993fecbf334cfa8508f7b4a429e449d40
student <student@ur.edu.pl>
2024-11-23 15:08:03 +0100

    Pierwszy commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// rm -rf * && rm -rf .* && git clone https://github.com/Yohop/algo-mat.git .
+
+void print_array(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    // printf("Hello, World!");
+   
+    return 0;
+}
